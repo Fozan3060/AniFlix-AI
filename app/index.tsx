2bb174f383e1a67f08bbc6d4cb83e1c01cbfa4e9
Fozan3060 <98460987+Fozan3060@users.noreply.github.com>
2025-10-13 21:12:11 +0500

    feat: setup theme

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="text-2xl bg-red-200">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-2xl text-light-100 bg-red-200">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#0F0D23",
+        accent: "#AB8BFF",
+        liner_background: "linear-gradient(90deg, #D6C7FF 0%, #AB8BFF 100%)",
+        light:{
+          100: "#D6C7FF",
+          200: "#D1C0FF",
+          300: "#A8B5DB",
+          400: "#ECECFF",
+        },
+        dark:{
+          100: "#9CA4AB",
+          200: "#121212",
+        }
+      },
+    },
   },
   plugins: [],
 }
